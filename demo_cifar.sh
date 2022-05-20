4ec5a184c6645f11d24baf226576d399a3fb7c25
Yiyou Sun <sunyiyou@cs.wisc.edu>
2022-05-19 22:28:24 -0500

    update

--- demo_cifar.sh
+++ demo_cifar.sh
@@ -3,8 +3,3 @@ python run_cifar.py --in-dataset CIFAR-10  --out-datasets SVHN LSUN iSUN dtd pla
 
 python feat_extract.py --in-dataset CIFAR-10  --out-datasets LSUN_FIX ImageNet_FIX Imagenet_resize CIFAR-100 --name resnet18-supcon  --model-arch resnet18-supcon
 python run_cifar.py --in-dataset CIFAR-10  --out-datasets LSUN_FIX ImageNet_FIX Imagenet_resize CIFAR-100 --name resnet18-supcon  --model-arch resnet18-supcon
-
-
-
-python feat_extract.py --in-dataset CIFAR-100  --out-datasets SVHN LSUN iSUN dtd places365 --name resnet34-supcon2  --model-arch resnet34-supcon
-python run_cifar.py --in-dataset CIFAR-100  --out-datasets SVHN LSUN iSUN dtd places365 --name resnet34-supcon2  --model-arch resnet34-supcon
